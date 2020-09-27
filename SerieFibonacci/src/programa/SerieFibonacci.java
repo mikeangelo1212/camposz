0245e66dd54ea74d454345d18cf1b970f71e3f24
mikeangelo1212 <70547488+mikeangelo1212@users.noreply.github.com>
2020-09-26 20:21:53 -0500

    Add files via upload

---
+++ SerieFibonacci/src/programa/SerieFibonacci.java
@@ -0,0 +1,144 @@
+package programa;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class SerieFibonacci extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+
+	/**
+	 * Lanza la aplicacion.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					SerieFibonacci frame = new SerieFibonacci();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+		//DefaultListModel listModel;
+		private JTextField textField_3;
+	/**
+	 * Crea la ventana.
+	 */
+	public SerieFibonacci() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 424, 305);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Insertar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) 
+			{
+				
+				//listModel =new DefaultListModel();
+				String texto1 = textField.getText();
+				String texto2 =  textField_1.getText();
+				
+				int x=0,y=0;
+				try
+				{
+					x = Integer.parseInt(texto1);
+					y = Integer.parseInt(texto2);
+				}catch(NumberFormatException ex)
+				{
+					System.err.println("No se puede convertir a numero");
+					ex.printStackTrace();
+				}
+					textField_3.setText(texto1+" \n"+texto2+" \n");
+					
+		        int m = Integer.parseInt(textField_2.getText());
+		        int a=1;
+		        while(a<m)
+		        {
+		            int z= x+y;
+		            String anterior = textField_3.getText();
+		            textField_3.setText(anterior+" \n"+z);
+		            x=y;
+		            y=z;
+		            a++;
+		        }
+			}
+		});
+		btnNewButton.setBounds(246, 20, 89, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Despejar");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				textField.setText("");
+				textField_1.setText("");
+				textField_2.setText("");
+				textField_3.setText("");
+				System.out.println("Limpiado");
+				
+			}
+		});
+		btnNewButton_1.setBounds(246, 51, 89, 23);
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Salir");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) 
+			{
+				/*boton para cerrar SerieFibonacci*/
+				System.exit(WIDTH);
+			}
+		});
+		btnNewButton_2.setBounds(246, 90, 89, 23);
+		contentPane.add(btnNewButton_2);
+		
+		JLabel lblNewLabel = new JLabel("Valor inicial 1");
+		lblNewLabel.setBounds(43, 24, 109, 14);
+		contentPane.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(116, 21, 86, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(116, 52, 86, 20);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Valor inicial 2");
+		lblNewLabel_1.setBounds(43, 55, 109, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("No. de iteraciones");
+		lblNewLabel_2.setBounds(43, 94, 129, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(137, 91, 36, 20);
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(29, 137, 344, 118);
+		textField_3.setEditable(false);
+		contentPane.add(textField_3);
+		textField_3.setColumns(10);
+	}
+}
